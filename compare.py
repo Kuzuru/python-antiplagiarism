176db41ae22b3992cdb2369bac69d0e689bd9a87
Artem Kiselev <kuzuru.dev@gmail.com>
2023-01-04 20:51:41 +0500

    feat: Implemented read_and_compare method

--- compare.py
+++ compare.py
@@ -23,7 +23,7 @@ class ArgsWorker:
 
     # Defines the string representation of the Boolean
     @staticmethod
-    def str_to_bool(value) -> bool:
+    def str_to_bool(value: str) -> bool:
         if value.lower() in {"false", "f", "0", "no", "n"}:
             return False
         elif value.lower() in {"true", "t", "1", "yes", "y"}:
@@ -44,7 +44,7 @@ def get_code_syntax_tree(filename) -> str:
     return normalized_code
 
 
-def levenstein(str_1, str_2):
+def levenstein(str_1: str, str_2: str) -> int:
     n, m = len(str_1), len(str_2)
     if n > m:
         str_1, str_2 = str_2, str_1
@@ -66,15 +66,33 @@ def levenstein(str_1, str_2):
     return current_row[n]
 
 
-def read_and_compare(file_orig, file_copy):
+def read_and_compare(is_verbose: bool, scores_output: str, file_orig: str, file_copy: str) -> None:
     code_orig = get_code_syntax_tree(file_orig)
     code_copy = get_code_syntax_tree(file_copy)
 
-    # Calculate the Levenshtein distance between the two strings
     distance = levenstein(code_orig, code_copy)
 
-    # Print the distance, or save it to a file if verbose output is disabled
-    print(distance)
+    # - If distance == 0, the files are identical,
+    # if the distance increases relative to 0,
+    # they become more different
+    # - So (distance / len(code_orig) is DIFFERENCE
+    # and (1 - DIFFERENCE) is SIMILARITY
+    similarity = 1 - (distance / len(code_orig))
+
+    output = f"[{file_orig} // {file_copy}]\r\nDistance: {distance}\r\nSimilarity: {similarity}\r\n\n"
+
+    # Print info on screen if verbose mode is on
+    if is_verbose:
+        if distance != 0 and len(code_orig) != 0:
+            print(output)
+        elif distance == 0 and len(code_orig) != 0:
+            print(f"[{file_orig} // {file_copy}]\r\nFiles are identical")
+
+    with open(scores_output, "a") as f:
+        if distance != 0 and len(code_orig) != 0:
+            f.write(output)
+        elif distance == 0 and len(code_orig) != 0:
+            f.write(f"[{file_orig} // {file_copy}]\r\nFiles are identical")
 
 
 def main():
@@ -83,7 +101,7 @@ def main():
     with open(args.get.input_list, "r") as f:
         for line in f:
             filenames = line.strip().split()
-            read_and_compare(filenames[0], filenames[1])
+            read_and_compare(args.get.verbose, args.get.scores_output, filenames[0], filenames[1])
 
 
 if __name__ == "__main__":
