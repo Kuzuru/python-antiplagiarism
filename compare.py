39155addc328bcba46ddf954cd439bd236330584
Artem Kiselev <kuzuru.dev@gmail.com>
2023-01-04 19:24:03 +0500

    feat: Implemented Levenstein alg

--- compare.py
+++ compare.py
@@ -29,6 +29,28 @@ def parse_args():
     return parser.parse_args()
 
 
+def levenstein(str_1, str_2):
+    n, m = len(str_1), len(str_2)
+    if n > m:
+        str_1, str_2 = str_2, str_1
+        n, m = m, n
+
+    current_row = range(n + 1)
+
+    for i in range(1, m + 1):
+        previous_row, current_row = current_row, [i] + [0] * n
+
+        for j in range(1, n + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+
+            if str_1[j - 1] != str_2[i - 1]:
+                change += 1
+
+            current_row[j] = min(add, delete, change)
+
+    return current_row[n]
+
+
 def read_and_compare(file_orig, file_copy):
     print(f"Got files: {file_orig} and {file_copy}")
 
