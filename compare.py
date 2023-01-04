21e816fb487abb1b277374efd8288eba520e10c4
Artem Kiselev <kuzuru.dev@gmail.com>
2023-01-04 21:34:03 +0500

    feat: Implemented Antiplagiarism class

--- compare.py
+++ compare.py
@@ -32,76 +32,84 @@ class ArgsWorker:
         raise ValueError(f"{value} is not a valid boolean value")
 
 
-def get_code_syntax_tree(filename) -> str:
-    with open(filename, "r") as f:
-        code = f.read()
+class Antiplagiarism:
+    def __init__(self):
+        self.args = ArgsWorker()
 
-    node = ast.parse(code)
+    @staticmethod
+    def get_code_syntax_tree(filename) -> str:
+        with open(filename, "r") as f:
+            code = f.read()
 
-    # TODO: Replace the use of ast.dump() with something better
-    normalized_code = ast.dump(node)
+        node = ast.parse(code)
 
-    return normalized_code
+        # TODO: Replace the use of ast.dump() with something better
+        normalized_code = ast.dump(node)
 
+        return normalized_code
 
-def levenstein(str_1: str, str_2: str) -> int:
-    n, m = len(str_1), len(str_2)
-    if n > m:
-        str_1, str_2 = str_2, str_1
-        n, m = m, n
+    @staticmethod
+    def levenstein(str_1: str, str_2: str) -> int:
+        n, m = len(str_1), len(str_2)
+        if n > m:
+            str_1, str_2 = str_2, str_1
+            n, m = m, n
 
-    current_row = range(n + 1)
+        current_row = range(n + 1)
 
-    for i in range(1, m + 1):
-        previous_row, current_row = current_row, [i] + [0] * n
+        for i in range(1, m + 1):
+            previous_row, current_row = current_row, [i] + [0] * n
 
-        for j in range(1, n + 1):
-            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            for j in range(1, n + 1):
+                add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
 
-            if str_1[j - 1] != str_2[i - 1]:
-                change += 1
+                if str_1[j - 1] != str_2[i - 1]:
+                    change += 1
 
-            current_row[j] = min(add, delete, change)
+                current_row[j] = min(add, delete, change)
 
-    return current_row[n]
+        return current_row[n]
 
+    # Reads and compares two files
+    def read_and_compare(self, file_orig: str, file_copy: str) -> None:
+        code_orig = self.get_code_syntax_tree(file_orig)
+        code_copy = self.get_code_syntax_tree(file_copy)
 
-def read_and_compare(is_verbose: bool, scores_output: str, file_orig: str, file_copy: str) -> None:
-    code_orig = get_code_syntax_tree(file_orig)
-    code_copy = get_code_syntax_tree(file_copy)
+        distance = self.levenstein(code_orig, code_copy)
 
-    distance = levenstein(code_orig, code_copy)
+        # - If distance == 0, the files are identical,
+        # if the distance increases relative to 0,
+        # they become more different
+        # - So (distance / len(code_orig) is DIFFERENCE
+        # and (1 - DIFFERENCE) is SIMILARITY
+        similarity = 1 - (distance / len(code_orig))
 
-    # - If distance == 0, the files are identical,
-    # if the distance increases relative to 0,
-    # they become more different
-    # - So (distance / len(code_orig) is DIFFERENCE
-    # and (1 - DIFFERENCE) is SIMILARITY
-    similarity = 1 - (distance / len(code_orig))
+        output = f"[ {file_orig} || {file_copy} ]\nLevenstein: {distance}\nSimilarity: {similarity}\n\n"
 
-    output = f"[ {file_orig} || {file_copy} ]\nLevenstein: {distance}\nSimilarity: {similarity}\n\n"
+        # Print info on screen if verbose mode is on
+        if self.args.get.verbose:
+            if distance != 0 and len(code_orig) != 0:
+                print(output)
+            elif distance == 0 and len(code_orig) != 0:
+                print(f"[ {file_orig} || {file_copy} ]\nFiles are identical\n\n")
 
-    # Print info on screen if verbose mode is on
-    if is_verbose:
-        if distance != 0 and len(code_orig) != 0:
-            print(output)
-        elif distance == 0 and len(code_orig) != 0:
-            print(f"[ {file_orig} || {file_copy} ]\nFiles are identical\n\n")
+        with open(self.args.get.scores_output, "a") as f:
+            if distance != 0 and len(code_orig) != 0:
+                f.write(output)
+            elif distance == 0 and len(code_orig) != 0:
+                f.write(f"[ {file_orig} || {file_copy} ]\nFiles are identical\n\n")
 
-    with open(scores_output, "a") as f:
-        if distance != 0 and len(code_orig) != 0:
-            f.write(output)
-        elif distance == 0 and len(code_orig) != 0:
-            f.write(f"[ {file_orig} || {file_copy} ]\nFiles are identical\n\n")
+    # Reads and compares list of files
+    def compare_inputs(self, input_list):
+        with open(input_list, "r") as f:
+            for line in f:
+                filenames = line.strip().split()
+                self.read_and_compare(filenames[0], filenames[1])
 
 
 def main():
-    args = ArgsWorker()
-
-    with open(args.get.input_list, "r") as f:
-        for line in f:
-            filenames = line.strip().split()
-            read_and_compare(args.get.verbose, args.get.scores_output, filenames[0], filenames[1])
+    apg = Antiplagiarism()
+    apg.compare_inputs(apg.args.get.input_list)
 
 
 if __name__ == "__main__":
