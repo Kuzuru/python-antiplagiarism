bc1ab4dd0daaeaad714271c2e856ce339ce8fe1b
Artem Kiselev <kuzuru.dev@gmail.com>
2023-01-04 18:30:13 +0500

    feat: Added argument processing

--- compare.py
+++ compare.py
@@ -1,5 +1,36 @@
+import argparse
+
+
+# Defines the string representation of the Boolean
+def str_to_bool(value):
+    if value.lower() in {'false', 'f', '0', 'no', 'n'}:
+        return False
+    elif value.lower() in {'true', 't', '1', 'yes', 'y'}:
+        return True
+
+    raise ValueError(f'{value} is not a valid boolean value')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Assessing the similarity of two Python scripts")
+
+    parser.add_argument("input_list", type=str, help="Input file with list of files to compare")
+    parser.add_argument("scores_output", type=str, help="Output file of the similarity estimation of program texts")
+
+    parser.add_argument(
+        "-v", "--verbose",
+        type=str_to_bool,
+        nargs='?',
+        const=True,
+        default=False,
+        help="Verbose output"
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    print("Hello World!")
+    parse_args()
 
 
 if __name__ == "__main__":
